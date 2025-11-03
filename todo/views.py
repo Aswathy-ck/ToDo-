2e7c83288352c848bf9311c4fcc9e4c4c5321856
Aswathy-ck <143797922+Aswathy-ck@users.noreply.github.com>
2025-11-03 21:02:24 +0530

    add task functionality

---
+++ todo/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Task
+
+# Create your views here.
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return HttpResponse('The form is submitted')
