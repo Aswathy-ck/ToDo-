dbbf566a1d6245f9ec5636a22dfa5c219be0015d
Aswathy-ck <143797922+Aswathy-ck@users.noreply.github.com>
2025-11-04 19:18:12 +0530

    completed

--- todo/views.py
+++ todo/views.py
@@ -7,22 +7,25 @@ from .models import Task
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
-    return HttpResponse('The form is submitted')
+    return redirect('home')
+
 def mark_as_done(request,pk):
     task = get_object_or_404(Task,pk=pk)
     task.is_completed=True
     task.save()
     return redirect('home')
+
 def mark_as_undone(request,pk):
     task = get_object_or_404(Task,pk=pk)
     task.is_completed=False
     task.save()
     return redirect('home')
+
 def delete(request,pk):
     task=get_object_or_404(Task,pk=pk)
     task.delete()
-    
     return redirect('home')
+
 def edit_task(request,pk):
     get_task = get_object_or_404(Task,pk=pk)
     if request.method== 'POST':
